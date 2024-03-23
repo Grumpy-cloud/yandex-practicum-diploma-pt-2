738342a24b6ee8a169191ece02d2ec743bd2068d
serleh <serleh20000@gmail.com>
2024-03-23 16:17:51 +0300

    add get order

---
+++ src/data.py
@@ -0,0 +1,16 @@
+headers = {
+    'Content-type': 'application/json'
+}
+
+
+order_creation_body = {
+    'firstName': 'Сергей',
+    'lastName': 'Макаров',
+    'address': 'Москва',
+    'metroStation': 1,
+    'phone': '+79998887766',
+    'rentTime': 1,
+    'deliveryDate': '2024-03-30',
+    'comment': 'Комментарий',
+    'color': []
+}
